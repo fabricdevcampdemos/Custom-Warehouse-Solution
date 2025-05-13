91ed24773fbc3d6958641699a9c8e101287b8855
Ted Pattison (DevCamp) <ted@fabricdevcamp.net>
2025-05-13 17:19:08 -0400

    default message

---
+++ src/event_test.py
@@ -0,0 +1,28 @@
+"""Event Test"""
+
+import os
+import json
+MESSAGE = 'Hello World fom the Dev Camp Event Test'
+
+print(MESSAGE)
+
+print('')
+print('GITHUB_CONTEXT:')
+
+context = json.loads(os.getenv('GITHUB_CONTEXT'))
+
+print(f"event_name: {context['event_name']}")
+
+print(f"ref: {context['ref']}")
+
+print(f"sha: {context['sha']}")
+
+print(f"workflow: {context['workflow']}")
+
+print(f"job: {context['job']}")
+
+if 'event' in context and 'master_branch' in context['event']:
+    print(f"event master_branch: {context['event']['master_branch']}")
+
+
+print( json.dumps(context, indent=4 ) )
